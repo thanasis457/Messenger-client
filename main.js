511a58ea39c76d43c4431ef187eb712b75672491
Athanasios Taprantzis <thatapras@hotmail.gr>
2019-11-12 17:43:02 +0200

    Fixed bug that prevented the start in Windows and Linux

--- main.js
+++ main.js
@@ -16,11 +16,6 @@ function createWindow () {
     },
     icon : path.join(__dirname,"ms.icns")
   })
-  const app = electron.app;
-  const image = electron.nativeImage.createFromPath(
-    path.join(__dirname,"mes.png")
-  );
-  app.dock.setIcon(image);
   win.loadFile('index.html');
   // and load the index.html of the app.
 
@@ -40,7 +35,14 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready',()=>{
+  createWindow();
+  const app = electron.app;
+  const image = electron.nativeImage.createFromPath(
+    path.join(__dirname,"mes.png")
+  );
+  app.dock.setIcon(image);
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -55,7 +57,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
 })
 
